c836e06ce57b6c75ed590042807270e017f1626e
Salman <salman.hashmi2@mail.mcgill.ca>
2017-07-26 23:43:29 -0400

    clean up constants in events module

--- code/events.py
+++ code/events.py
@@ -1,4 +1,12 @@
-"""Module for rover events."""
+"""
+Functions for listening to events in the rover environment.
+
+NOTE:
+- angles are in degrees
+- distances are in meters
+- speeds are in meters/second
+
+"""
 
 __author__ = 'Salman Hashmi'
 __license__ = 'BSD License'
@@ -6,72 +14,140 @@ __license__ = 'BSD License'
 
 import numpy as np
 
-
-def velocity_exceeded(Rover):
-    """Check if velocity is under max_vel."""
-    return Rover.vel < Rover.max_vel
+from constants import TO_DEG
 
 
-def front_path_clear(Rover):
-    """Check if sufficient room ahead."""
-    return len(Rover.nav_angles) >= Rover.go_forward
+def velocity_exceeded(Rover, max_vel=2.0):
+    """
+    Check if velocity is under max_vel.
+
+    Keyword arguments:
+    max_vel -- maximum velocity in meters/second
+    """
+    return Rover.vel < max_vel
 
 
-def left_path_clear(Rover):
-    """Check if sufficient room on left."""
-    return len(Rover.nav_angles_left) >= 1500
+def front_path_clear(Rover, safe_pixs=500):
+    """
+    Check if sufficient room ahead.
+
+    Keyword arguments:
+    safe_pixs -- minimum number of pixels in front to deem front path clear
+    """
+    nav_pixs_front = len(Rover.nav_angles)
+    return nav_pixs_front >= safe_pixs
 
 
-def pointed_at_nav(Rover):
-    """Check if rover pointed at +/- some nav heading."""
-    return (np.mean(Rover.nav_angles) <= 0.3 and
-            np.mean(Rover.nav_angles) >= -0.3)
+def left_path_clear(Rover, safe_pixs=1500):
+    """
+    Check if sufficient room on left.
+
+    Keyword arguments:
+    safe_pixs -- minimum number of pixels on left to deem left path clear
+    """
+    nav_pixs_left = len(Rover.nav_angles_left)
+    return nav_pixs_left >= safe_pixs
 
 
-def pointed_along_wall(Rover):
-    """Check if rover has sufficient nav pixels along left wall."""
-    go_forward_wall = 500
-    return (len(Rover.nav_angles_left) > go_forward_wall and
-            np.mean(Rover.nav_angles_left*180 / np.pi)-10 > 0)
+def pointed_at_nav(Rover, angle_limit=17):
+    """
+    Check if rover is pointed within some range of average nav heading.
+
+    Keyword arguments:
+    angle_limit -- angle range limit for nav angles (degrees)
+    """
+    nav_heading = np.mean(Rover.nav_angles)*TO_DEG
+    return -angle_limit <= nav_heading <= angle_limit
 
 
-def deviated_from_wall(Rover):
-    """Check if rover has deviated from wall beyond specified limit."""
-    wall_angle_limit = 25
-    return np.mean(Rover.nav_angles_left*180/np.pi) > wall_angle_limit
+def pointed_along_wall(Rover, safe_pixs=500, wall_angle_bias=-10):
+    """
+    Check if rover is pointing along left wall at some safe distance.
+
+    Keyword arguments:
+    safe_pixs --  minimum number of pixels to keep from wall
+    wall_angle_bias -- to bias rover heading for pointing along wall (degrees)
+    """
+    nav_pixs_left = len(Rover.nav_angles_left)
+    nav_heading_left = np.mean(Rover.nav_angles_left)*TO_DEG + wall_angle_bias
+
+    return (nav_pixs_left >= safe_pixs
+            and nav_heading_left > 0)
 
 
-def at_front_obstacle(Rover):
-    """Check if rover is up against some obstacle."""
-    front_stop_forward = 600
-    return len(Rover.nav_angles) < front_stop_forward
+def deviated_from_wall(Rover, max_angle_wall=25):
+    """
+    Check if rover has deviated from left wall beyond specified angle limit.
+
+    Keyword arguments:
+    max_angle_wall --  maximum allowed angle from left wall (degrees)
+    """
+    nav_heading_left = np.mean(Rover.nav_angles_left)*TO_DEG
+    return nav_heading_left > max_angle_wall
 
 
-def at_left_obstacle(Rover):
-    """Check if obstacle is left of rover."""
-    return len(Rover.nav_angles_left) < Rover.stop_forward
+def at_front_obstacle(Rover, safe_pixs=600):
+    """
+    Check if rover is up against some obstacle on the front.
+
+    Keyword arguments:
+    safe_pixs -- minimum number of pixels to keep from front obstacles
+    """
+    nav_pixs_front = len(Rover.nav_angles)
+    return nav_pixs_front < safe_pixs
 
 
-def sample_on_left(Rover):
-    """Check if sample spotted on the left."""
-    return len(Rover.rock_angles) >= 1 and np.mean(Rover.rock_angles) >= 0
+def at_left_obstacle(Rover, safe_pixs=50):
+    """
+    Check if obstacle is to the left of rover.
+
+    Keyword arguments:
+    safe_pixs -- minimum number of pixels to keep from left obstacles
+    """
+    nav_pixs_left = len(Rover.nav_angles_left)
+    return nav_pixs_left < safe_pixs
 
 
-def sample_right_close(Rover):
-    """Check if sample on the right and close by."""
-    return np.mean(Rover.rock_angles) > -0.3 and np.mean(Rover.rock_dists) < 75
+def sample_on_left(Rover, min_left_angle=0.0):
+    """Check if a sample is spotted on the left.
+
+    Keyword arguments:
+    min_left_angle -- only rocks to the left/above this are considered
+    """
+    rock_heading = np.mean(Rover.rock_angles)*TO_DEG
+    return rock_heading >= min_left_angle
+
+
+def sample_right_close(Rover, rock_dist_limit=75, max_right_angle=17):
+    """
+    Check if a nearby sample is spotted on the right.
+
+    Keyword arguments:
+    rock_dist_limit -- only rocks below this limit are considered
+    max_right_angle -- only rocks to left/above of this are considered
+    """
+    rock_heading = np.mean(Rover.rock_angles)*TO_DEG
+    rock_distance = np.mean(Rover.rock_dists)
+
+    return (rock_heading > -max_right_angle
+            and rock_distance < rock_dist_limit)
 
 
 def sample_in_view(Rover):
-    """Check if sample still in view."""
-    return len(Rover.rock_angles) >= 1
+    """Check if rock sample still in view."""
+    rock_pixs = len(Rover.rock_angles)
+    return rock_pixs >= 1
 
 
-def pointed_at_sample(Rover):
-    """Check if rover pointed at +/- some sample heading."""
-    return (len(Rover.rock_angles) >= 1 and
-               (np.mean(Rover.rock_angles) < 0.3 and
-                np.mean(Rover.rock_angles) > -0.3))
+def pointed_at_sample(Rover, angle_limit=17):
+    """
+    Check if rover is pointed within some range of average rock heading.
+
+    Keyword arguments:
+    angle_limit -- angle range limit for rock angles (radians)
+    """
+    rock_heading = np.mean(Rover.rock_angles)*TO_DEG
+    return -angle_limit < rock_heading < angle_limit
 
 
 def can_pickup_sample(Rover):
@@ -79,12 +155,13 @@ def can_pickup_sample(Rover):
     return Rover.near_sample and Rover.vel <= 0.1
 
 
-def completed_mission(Rover):
+def completed_mission(Rover, min_samples=6, min_mapped=95, max_time=700):
     """Check if rover has completed mission criteria."""
-    return ((Rover.samples_found >= 6 and Rover.perc_mapped >= 95.0) or
-            Rover.total_time >= 700)
+    return (Rover.samples_found >= min_samples
+            and Rover.perc_mapped >= min_mapped
+            ) or Rover.total_time >= max_time
 
 
-def reached_home(Rover):
+def reached_home(Rover, max_dist=5):
     """Check if rover has reached home after completing mission."""
-    return Rover.going_home and Rover.distance_to_home < 5
+    return Rover.going_home and Rover.distance_to_home < max_dist
